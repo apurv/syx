627dd45a6dbfafd0119edf01f7dfe64f674abf84
Apurv Parikh <apurvparikh1@gmail.com>
2015-09-30 10:37:53 -0400

    initial backend commit

---
+++ server/app/configure/parsing-middleware.js
@@ -0,0 +1,16 @@
+'use strict';
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+module.exports = function (app) {
+
+    // Important to have this before any session middleware
+    // because what is a session without a cookie?
+    // No session at all.
+    app.use(cookieParser());
+
+    // Parse our POST and PUT bodies.
+    app.use(bodyParser.json({limit: '50mb'}));
+    app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+
+};
