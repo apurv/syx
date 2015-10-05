20ad9ef8aa04fe44eaa9c949b73f3f662a9be30e
Apurv Parikh <apurvparikh1@gmail.com>
2015-10-05 15:44:29 -0400

    added a monkey patch to hide the non-prod react warnings

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var fs = require('fs');
 var mongoose = require('mongoose');
 var passport = require('passport');
+var _ = require('lodash');
 var secrets = require('./config/secrets');
+
 
 var app = express();
 
@@ -36,3 +38,23 @@ require('./config/routes')(app, passport);
 
 
 app.listen(app.get('port'));
+
+
+// PATCH - hides unnecessary warnings by React for non-prod environments
+var warn = console.warn;
+var warningFilterKey = function(warning) {
+    return warning.indexOf("Warning: owner-based and parent-based contexts differ") >= 0
+};
+var throttledWarn = _.throttle(function() {
+    warn.call(console,"Throttled warning about React owner/parent based contexts, see https://github.com/facebook/react/issues/4081 for reasons");
+    warn.apply(console, arguments);
+}, 60000);
+
+console.warn = function() {
+    if ( arguments && arguments.length > 0 && typeof arguments[0] === "string" && warningFilterKey(arguments[0]) ) {
+        throttledWarn.apply(throttledWarn,arguments);
+    }
+    else {
+        warn.apply(console, arguments);
+    }
+};
