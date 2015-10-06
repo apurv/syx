365fbcac3821aa1218fe77dfd896c12f9f8b3376
Apurv Parikh <apurvparikh1@gmail.com>
2015-10-06 11:17:15 -0400

    included bootstrap

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router';
 import UserActions from 'actions/UserActions';
 
 import styles from 'scss/components/_navigation';
+import Button from 'react-bootstrap/lib/Button';
 
 export default class Navigation extends React.Component {
 
@@ -13,16 +14,20 @@ export default class Navigation extends React.Component {
 
   render() {
     return (
-      <nav className={styles.navigation} role="navigation">
-          <Link to="/" className={styles.navigation__item + ' ' + styles['navigation__item--logo']} activeClassName={styles['navigation__item--active']}>syx</Link>
-          { this.props.UserStore.user.get('authenticated') ? (
-            <Link onClick={this._onLogout} className={styles.navigation__item} to="/logout">Logout</Link>
-          ) : (
-            <Link className={styles.navigation__item} to="/login">Log in</Link>
-          )}
-          <Link className={styles.navigation__item} to="/dashboard">Dashboard</Link>
-          <Link to="/about" className={styles.navigation__item} activeClassName={styles['navigation__item--active']}>About</Link>
-      </nav>
+      <div className="container">
+
+        <nav role="navigation" className="navbar navbar-default navbar-fixed-top">
+          <Button bsStyle="success" bsSize="large">Primary</Button>
+            <Link to="/" className={styles.navigation__item + ' ' + styles['navigation__item--logo']} activeClassName={styles['navigation__item--active']}>syx</Link>
+            { this.props.UserStore.user.get('authenticated') ? (
+              <Link onClick={this._onLogout} className={styles.navigation__item} to="/logout">Logout</Link>
+            ) : (
+              <Link className={styles.navigation__item} to="/login">Log in</Link>
+            )}
+            <Link className={styles.navigation__item} to="/dashboard">Dashboard</Link>
+            <Link to="/about" className={styles.navigation__item} activeClassName={styles['navigation__item--active']}>About</Link>
+        </nav>
+      </div>
     );
   }
 
--- app/helmconfig.js
+++ app/helmconfig.js
@@ -20,11 +20,11 @@ const config = {
     // Add to homescreen for Chrome on Android
     { "rel": "icon", "href": favicon },
     { "rel": "icon", "sizes": "192x192", "href": chromecon },
-    // Add to homescreen for Safari on IOS 
+    // Add to homescreen for Safari on IOS
     { "rel": "apple-touch-icon", "sizes": "152x152", applecon },
     { "rel": "stylesheet", "href": "https://fonts.googleapis.com/css?family=Roboto+Condensed", "type": "text/css" },
     { "rel": "stylesheet", "href": "/assets/styles/main.css" }
-    // SEO: If your mobile URL is different from the desktop URL, 
+    // SEO: If your mobile URL is different from the desktop URL,
     // add a canonical link to the desktop page https://developers.google.com/webmasters/smartphone-sites/feature-phones
     // { "rel": "canonical", "href": "http://www.example.com/" }
   ],
@@ -33,17 +33,17 @@ const config = {
     // Setting IE=edge tells Internet Explorer to use the latest engine to render the page and execute Javascript
     { "http-equiv": "X-UA-Compatible", "content": "IE=edge" },
     //  Meta descriptions are commonly used on search engine result pages to display preview snippets for a given page.
-    { "name": "description", "content": "An isomorphic React alt boilerplate with Express and MongoDB deployable to Heroku" },
-    // Mobile Safari introduced this tag to let web developers control the viewport's size and scale 
-    // The width property controls the size of the viewport, the initial-scale property controls 
+    { "name": "description", "content": "Syx is a blogging platform for technical learning." },
+    // Mobile Safari introduced this tag to let web developers control the viewport's size and scale
+    // The width property controls the size of the viewport, the initial-scale property controls
     // the zoom level when the page is first loaded
     { "name": "viewport", "content": "width=device-width, initial-scale=1" },
     // Add to homescreen for Chrome on Android
     { "name": "mobile-web-app-capable", "content": "yes" },
-    // Add to homescreen for Safari on IOS 
+    // Add to homescreen for Safari on IOS
     { "name": "apple-mobile-web-app-capable", "content": "yes" },
     { "name": "apple-mobile-web-app-status-bar-style", "content": "black" },
-    { "name": "apple-mobile-web-app-title", "content": "React Webpack Node" },
+    { "name": "apple-mobile-web-app-title", "content": "syx" },
     // Tile icon for Win8 (144x144 + tile color)
     { "name": "msapplication-TileImage", "content": mscon },
     { "name": "msapplication-TileColor", "content": "#3372DF" }
