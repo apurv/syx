724840ad2f51d199d3142ecb1cd89d6eaaca7c88
Apurv Parikh <apurvparikh1@gmail.com>
2015-10-13 16:11:12 -0400

    merge hotfix

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -7,10 +7,7 @@ import CardsDisplay from './CardsDisplay.jsx';
 require('bootstrap/dist/css/bootstrap.min.css');
 import { Router, Route, Link } from 'react-router';
 
-import articleStore from '../stores/article.store';
-import AppDispatcher from '../dispatcher/dispatcher';
-
-// import ArticleStore from '../stores/article.store';
+import ArticleStore from '../stores/article.store';
 import AppDispatcher from '../dispatcher/dispatcher';
 
 var data =  ['card1', 'card2', 'card3'];
