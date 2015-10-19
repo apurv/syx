9f7f4a7aa78526799208c7c4b25efed5f593a535
Apurv Parikh <apurvparikh1@gmail.com>
2015-10-19 11:42:22 -0400

    added viewpanel jsx

--- app/components/Viewer.jsx
+++ app/components/Viewer.jsx
@@ -6,6 +6,7 @@ import articleStore from '../stores/article.store';
 import AppDispatcher from '../dispatcher/dispatcher';
 import ArticleStore from '../stores/article.store';
 import marked from 'marked';
+import HeadersPanel from './HeadersPanel';
 require('bootstrap/dist/css/bootstrap.min.css');
 
 
@@ -84,51 +85,50 @@ export default class Viewer extends Component {
 
 		 return (
 			 <div className="row">
-				 <div id="syx-viewpanel" class="syx-view-intro">
+				 <div id="syx-viewpanel" class="syx-view-intro syx-intro-container" style={{ marginTop: '50px' }}>
+
 		 			<header class="header">
-		 				<div class="bg-img"><img src="./1.jpg" alt="Background Image" /></div>
+		 				<div class="bg-img"><img src="http://i.imgur.com/67EjwuD.jpg" alt="Background Image" /></div>
 		 				<div class="title">
 		 					<h1>Supernova Remnants</h1>
 		 					<p class="subline">Inspiration for Article Intro Effects</p>
 		 					<p>by <strong>Apurv Parikh</strong> &#8212; Posted in <strong>Web Fundamentals</strong> on <strong>October 18, 2015</strong></p>
 		 				</div>
 		 			</header>
-		 			<button class="trigger" data-info="Click to see the header effect"><span>Trigger</span></button>
-		 			<article class="content">
 
+
+		 			<article class="content">
+							{ /* HEADERS PANEL */}
+							<HeadersPanel article={this.state.article} />
+
+							{ /* ARTICLE PANEL */}
+							<div className="col-md-8" onDoubleClick={this.handleEditing.bind(this)} style={editStyle}>
+								<h1>Article</h1>
+								<div dangerouslySetInnerHTML={this.markdownify()}></div>
+							</div>
+
+							<div className="col-md-8" style={viewStyle}>
+								<h1>Article</h1>
+								<textarea type="text"
+									className="form-control"
+									ref="articleTextArea"
+									style={{height: '90vh'}}
+									value={value}
+									onChange={this.handleChange.bind(this)}
+									onBlur={this.handleEditingDone.bind(this)}>
+								</textarea>
+							</div>
+
+							{ /* TOOLS PANEL */}
+							<div className="col-md-2">
+								<h3>Control Panel</h3>
+							</div>
 		 			</article>
 		 		</div>
 			</div>
 		);
-			<div className="container" style={{ marginTop: '50px' }}>
-				<div className="row">
-					<LeftSidebar article={this.state.article} />
-artis<div className="col-md-2">
-						<h3>Side-Bar</h3>
-					</div>
-					<div className="col-md-8" onDoubleClick={this.handleEditing.bind(this)} style={editStyle}>
-						<h1>Article</h1>
-						<div dangerouslySetInnerHTML={this.markdownify()}></div>
-					</div>
-
-					<div className="col-md-8" style={viewStyle}>
-						<h1>Article</h1>
-						<textarea type="text"
-							className="form-control"
-							ref="articleTextArea"
-							style={{height: '90vh'}}
-							value={value}
-							onChange={this.handleChange.bind(this)}
-							onBlur={this.handleEditingDone.bind(this)}>
-						</textarea>
-					</div>
-
-					<div className="col-md-2">
-						<h3>Control Panel</h3>
-					</div>
-
-				</div>
-			</div>
-		);
 	}//end render
 }
+
+
+// 		 			<button class="trigger" data-info="Click to see the header effect"><span>Trigger</span></button>
