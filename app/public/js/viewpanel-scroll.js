fb73a8629555664cc293aeb293cee4edda57d4d7
Apurv Parikh <apurvparikh1@gmail.com>
2015-10-20 16:44:03 -0400

    minor cleanup

--- app/public/js/viewpanel-scroll.js
+++ app/public/js/viewpanel-scroll.js
@@ -10,36 +10,28 @@ function viewpanelScroll(add) {
       isAnimating,
       container = document.getElementById('syx-viewpanel');
 
-  console.log("container", container);
-
   if (container && add) {
-    console.log("%c ADDED LISTENER", "background: #1c1c1c; color: deeppink");
     window.scrollTo(0, 0);
     document.onscroll = scrollPage;
   } else {
-    console.log("%c REMOVED LISTENER", "background: #1c1c1c; color: deeppink");
     document.onscroll = null;
   }
 
 
 
   function disable_scroll() {
-    console.log("%c disable_scroll invoked", "background: #1c1c1c; color: deepskyblue");
     window.onmousewheel = document.onmousewheel = null;
   }
 
   function enable_scroll() {
-    console.log("%c enable_scroll invoked", "background: #1c1c1c; color: deeppink");
     window.onmousewheel = document.onmousewheel = document.onkeydown = document.body.ontouchmove = null;
   }
 
   function scrollY() {
-    console.log("%c scrollY invoked", "background: #1c1c1c; color: deepskyblue", window.pageYOffset, docElem.scrollTop);
     return window.pageYOffset || docElem.scrollTop;
   }
 
   function scrollPage() {
-    console.log("%c scrollPage invoked", "background: #1c1c1c; color: white");
     scrollVal = scrollY();
 
     if (noscroll) {
@@ -65,7 +57,6 @@ function viewpanelScroll(add) {
   }
 
   function toggle(reveal) {
-    console.log("%c toggle invoked", "background: #1c1c1c; color: yellow", '');
     isAnimating = true;
 
     if (reveal) {
@@ -94,7 +85,6 @@ function viewpanelScroll(add) {
   disable_scroll();
 
   if (pageScroll) {
-    console.log("%c pageScroll IF CONDITION invoked", "background: #1c1c1c; color: yellow");
     isRevealed = true;
     classie.add(container, 'notrans');
     classie.add(container, 'modify');
